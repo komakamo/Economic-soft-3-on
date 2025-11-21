c8b99446c94fd1874642e2d2933ddb3c4a24e161
SHOBU SHOGO <120190757+komakamo@users.noreply.github.com>
2025-11-21 16:29:47 +0900

    Make Vite base configurable

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/Economic-soft-3-on/',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: env.BASE_URL ?? '/',
+    plugins: [react()],
+  };
 });
